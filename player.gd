d98b265eb43c43832ee907687af6c756e43942ed
alixbarreaux <alixbarreaux@github.com>
2023-10-18 23:34:39 +0200

    Feat: Add ability to climb for player
    Known bug: Pressing left or right + up or down makes the player fall through the platform while on the climbable and makes a 'jump' when on the ground at the bottom of the climbable

--- player.gd
+++ player.gd
@@ -3,14 +3,14 @@ extends CharacterBody2D
 
 
 @export var speed: float = 300.0
+@export var climb_speed: float = 150.0
 
 @export var gravity: float = 200.0
 @export var max_gravity: float = 1200.0
 
-var is_colliding_with_climbable: bool = false
+var direction: Vector2 = Vector2(0.0, 0.0)
 
-var input_direction_x: float = 0.0
-var input_direction_y: float = 0.0
+var is_colliding_with_climbable: bool = false
 
 
 @onready var animation_tree: AnimationTree = $AnimationTree
@@ -21,26 +21,22 @@ func _ready():
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	input_direction_x = Input.get_axis("move_left", "move_right")
-	input_direction_y = Input.get_axis("move_up", "move_down")
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
 
-var climb_speed: float = 150.0
 
 func _physics_process(delta: float) -> void:
-	
 	if is_colliding_with_climbable:
-#	if $LadderChecker.is_colliding():
-		#print("Raycast colliding!")
-		self.velocity.y = input_direction_y * climb_speed
+		self.velocity.y = direction.y * climb_speed
 	else:
-#		print("Raycast NOT colliding!!")
 		self.velocity.y += gravity
 		if self.velocity.y >= max_gravity:
 			self.velocity.y = max_gravity
 	
 	if self.is_on_floor():
-		self.velocity.x = input_direction_x * self.speed
+		self.velocity.x = direction.x * self.speed
 	
 	self.move_and_slide()
 	
-	#print("velocity: ", velocity)
+#	print("direction: ", direction)
+#	print("velocity: ", velocity)
