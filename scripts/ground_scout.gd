2de1082f6d7a746ff8febe454eb57da433b23842
alixbarreaux <alixbarreaux@github.com>
2023-10-20 19:15:12 +0200

    Feat: Add AI to scout
    Doesn't rotate when velocity changes for now

--- scripts/ground_scout.gd
+++ scripts/ground_scout.gd
@@ -2,5 +2,17 @@ extends CharacterBody2D
 class_name GroundScout
 
 
+@export var speed: float = 100.0
+
+@export var direction: Vector2 = Vector2(1.0, 0.0)
+
+
+@onready var obstacle_detector: RayCast2D = $ObstacleDetector
+
 func _physics_process(delta: float) -> void:
-	print(move_and_slide())
+	self.velocity.x = direction.x * self.speed
+	
+	if obstacle_detector.is_colliding():
+		self.direction.x = -1
+	
+	self.move_and_slide()
