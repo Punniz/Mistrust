901646ca47312bf31a565fd36493b6dd82546fab
JeffSabol <jeffreysabol345@gmail.com>
2023-10-20 22:58:59 -0400

    Added a pause to the clumb animation

--- run.gd
+++ run.gd
@@ -1,6 +1,8 @@
 class_name RunState
 extends State
 
+var is_paused_in_climb = false
+
 #@export var actor: Player
 @onready var sprite_node = $"../../Sprite2D"
 @onready var animation_state_machine = animation_tree.get("parameters/playback")
@@ -10,20 +12,31 @@ func _ready():
 
 func enter_state() -> void:
 	animation_state_machine.travel("run")
-	pass
+	is_paused_in_climb = false  # Reset the flag when entering this state
 
 func exit_state() -> void:
 	pass
 
-func _physics_process(delta): 
+func _physics_process(delta):
+	# If climbing
 	if actor.velocity.y != 0 and actor.is_colliding_with_climbable:
-		# todo make climb state
-		# exit run state go to climb state
-		animation_state_machine.travel("climb") # move into climb state
+		animation_state_machine.travel("climb")
+		is_paused_in_climb = false  # Reset the flag
+		animation_tree.active = true  # Resume the animation
+	# If paused while climbing
+	elif actor.velocity.y == 0 and actor.is_colliding_with_climbable:
+		if !is_paused_in_climb:  # Check the flag
+			animation_tree.set("parameters/climb/current_frame", 0)  # Set to the first frame
+			animation_tree.active = false  # Pause the animation
+			is_paused_in_climb = true  # Set the flag
+	# If running
 	elif actor.direction.x != 0:
 		animation_state_machine.travel("run")
+		is_paused_in_climb = false  # Reset the flag
+		animation_tree.active = true  # Resume the animation
 		
-	if actor.direction.x == -1: # if running left, flip the run animation horizontally
+	# Handle sprite flipping based on direction
+	if actor.direction.x == -1:  # if running left, flip the run animation horizontally
 		sprite_node.scale.x = -abs(sprite_node.scale.x)
-	elif actor.direction.x == 1: # if running right, put the run animation back 
+	elif actor.direction.x == 1:  # if running right, put the run animation back
 		sprite_node.scale.x = abs(sprite_node.scale.x)
