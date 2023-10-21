7b47e6c1b951f972e4913e1cfc46c9446ab1019b
JeffSabol <jeffreysabol345@gmail.com>
2023-10-21 10:43:06 -0400

    Fixed the run animation freezes when stopping on the ladder

--- run.gd
+++ run.gd
@@ -2,6 +2,7 @@ class_name RunState
 extends State
 
 var is_paused_in_climb = false
+var is_running = false
 
 #@export var actor: Player
 @onready var sprite_node = $"../../Sprite2D"
@@ -19,12 +20,12 @@ func exit_state() -> void:
 	pass
 
 func _physics_process(delta):
-	
 	var is_ray_hit = ray_cast.is_colliding()
 	
 	# Check for RayCast collision
-	if is_ray_hit and actor.velocity.y == 0 and actor.velocity.x == 0:
+	if is_ray_hit and actor.velocity.y == 0 and actor.velocity.x == 0 and not is_running:
 		animation_state_machine.travel("idle")
+		is_running = false
 			
 
 	
@@ -33,17 +34,20 @@ func _physics_process(delta):
 		animation_state_machine.travel("climb")
 		is_paused_in_climb = false  # Reset the flag
 		animation_tree.active = true  # Resume the animation
+		is_running = false
 	# If paused while climbing
-	elif actor.velocity.y == 0 and actor.is_colliding_with_climbable:
+	elif actor.velocity.y == 0 and actor.is_colliding_with_climbable and not is_running:
 		if !is_paused_in_climb:  # Check the flag
 			animation_tree.set("parameters/climb/current_frame", 0)  # Set to the first frame
 			animation_tree.active = false  # Pause the animation
-			is_paused_in_climb = true  # Set the flag
+			is_paused_in_climb = true 
+			is_running = false
 	# If running
 	elif actor.direction.x != 0:
 		animation_state_machine.travel("run")
 		is_paused_in_climb = false  # Reset the flag
 		animation_tree.active = true  # Resume the animation
+		is_running = true
 		
 	# Handle sprite flipping based on direction
 	if actor.direction.x == -1:  # if running left, flip the run animation horizontally
