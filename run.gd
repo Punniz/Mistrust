cb416636a8972dba2cc6d7db5566e819d0268576
JeffSabol <jeffreysabol345@gmail.com>
2023-10-22 19:39:42 -0400

    Added climbing sound

--- run.gd
+++ run.gd
@@ -3,11 +3,13 @@ extends State
 
 var is_paused_in_climb = false
 var is_running = false
+var is_climb_audio_playing = false
 
 #@export var actor: Player
 @onready var sprite_node = $"../../Sprite2D"
 @onready var animation_state_machine = animation_tree.get("parameters/playback")
 @onready var ray_cast = $"../../RayCast2D"
+@onready var climb_audio_player = $"../../ClimbStream"
 
 func _ready():
 	set_process(true)
@@ -26,16 +28,24 @@ func _physics_process(delta):
 	if is_ray_hit and actor.velocity.y == 0 and actor.velocity.x == 0 and not is_running:
 		animation_state_machine.travel("idle")
 		is_running = false
-			
-
+		# Stop climb audio when not climbing
+		climb_audio_player.stop()
+		# Set the flag to false
+		is_climb_audio_playing = false
 	
 	# If climbing
 	if actor.velocity.y != 0 and actor.is_colliding_with_climbable:
 		animation_state_machine.travel("climb")
-		actor.velocity.x = 0 # Stop sliding off the damn ladder when run->climb lol
-		is_paused_in_climb = false  # Reset the flag
-		animation_tree.active = true  # Resume the animation
+		actor.velocity.x = 0
+		is_paused_in_climb = false
+		animation_tree.active = true
 		is_running = false
+		
+		# Play the climbing sound only if it's not already playing
+		if not is_climb_audio_playing:
+			climb_audio_player.play()
+			is_climb_audio_playing = true  # Set the flag to true
+			
 	# If paused while climbing
 	elif actor.velocity.y == 0 and actor.is_colliding_with_climbable and not is_running:
 		if !is_paused_in_climb:  # Check the flag
@@ -43,12 +53,21 @@ func _physics_process(delta):
 			animation_tree.active = false  # Pause the animation
 			is_paused_in_climb = true 
 			is_running = false
+			# Stop climb audio when paused
+			climb_audio_player.stop()
+			# Set the flag to false
+			is_climb_audio_playing = false
+			
 	# If running
 	elif actor.direction.x != 0:
 		animation_state_machine.travel("run")
 		is_paused_in_climb = false  # Reset the flag
 		animation_tree.active = true  # Resume the animation
 		is_running = true
+		# Stop climb audio when running
+		climb_audio_player.stop()
+		# Set the flag to false
+		is_climb_audio_playing = false
 		
 	# Handle sprite flipping based on direction
 	if actor.direction.x == -1:  # if running left, flip the run animation horizontally
