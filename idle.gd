276023091350573a222743b3e6d85c114438d168
JeffSabol <jeffreysabol345@gmail.com>
2023-10-30 23:49:03 -0400

    cleaned up the idle state

--- idle.gd
+++ idle.gd
@@ -1,36 +1,18 @@
+# Handles what the protagonist should be doing when the character is waiting for user input. -Jeff
 class_name IdleState
 extends State
 
-
-#@export var actor: Player
-#
-#@export var animation_tree: AnimationTree
-#@onready var animation_tree_sm_playback: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
 @onready var animation_state_machine = animation_tree.get("parameters/playback")
-
 
 func _ready():
 	set_process(true)
-#	print(self.name, ": animation_tree: ", animation_tree)
-#	print(self.name, ": animation_node_sm : ", animation_tree_sm_playback)
 
+# Similarly, you could have methods for other animations
+func set_run_animation():
+	animation_state_machine.travel("run")
 
-func enter_state() -> void:
-	#print("enter idle state")
-
+func enter_state():
 	animation_state_machine.travel("idle")
-
-
-func update_state() -> void:
-#	animation_tree_sm_playback.travel("idle")
-	pass
-
-
+	
 func exit_state():
-	#print("exit idle state")
-	pass
-
-
-func _physics_process(_delta: float) -> void: # probably can delete
-	# Idle logic here
 	pass
